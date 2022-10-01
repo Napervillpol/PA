736c2524908cca7abe439b313803af5c87af052a
Naperville Nate <napervillepolitics@gmail.com>
2022-09-30 22:42:44 -0500

    added assign_race function

--- PA.py
+++ PA.py
@@ -15,6 +15,36 @@ class race:
         self.eday=eday;
         self.prov=prov;
 
+def assign_race(Dem,Rep,Dem_name,Rep_name):
+       
+    #Mail 
+    Dem_mail = Dem[['County Name','Mail Votes']];
+    Dem_mail.columns=['County',Dem_name];
+
+    Rep_mail = Rep[['County Name','Mail Votes']];
+    Rep_mail.columns=['County',Rep_name];
+    mail = Dem_mail.merge(Rep_mail, on='County')
+
+    #Election day
+    Dem_eday = Dem[['County Name','Election Day Votes']];
+    Dem_eday.columns=['County',Dem_name];
+
+    Rep_eday = Rep[['County Name','Election Day Votes']];
+    Rep_eday.columns=['County',Rep_name];
+    eday = Dem_eday.merge(Rep_eday, on='County')
+
+    #Provisonal
+    Dem_prov= Dem[['County Name','Provisional Votes']];
+    Dem_prov.columns=['County',Dem_name];
+
+    Rep_prov = Trump[['County Name','Provisional Votes']];
+    Rep_prov.columns=['County',Rep_name];
+    prov = Dem_prov.merge(Rep_prov, on='County')
+
+    Race = race(mail,eday,prov);
+    return Race;
+
+
 df = pd.read_csv('PA.csv');
 df2= pd.read_csv('PA_2020.csv');
 
@@ -48,8 +78,11 @@ prov = Biden_prov.merge(Trump_prov, on='County')
 
 President = race(mail,eday,prov);
 
+# 2022 Senate
+Fetterman= df.loc[(df['Office Name']  =='United States Senator' ) & (df['Party Name']  =='Democratic' )];
+Oz= df.loc[(df['Office Name']  =='United States Senator' ) & (df['Party Name']  =='Republican' )];
 
-Fetterman = df.loc[(df['Office Name']  =='United States Senator' ) & (df['Party Name']  =='Democratic' )];
+President = assign_race(Biden,Trump,"Biden","Trump");
+Senate = assign_race(Fetterman,Oz,"Fetterman","Oz");
 
-
-print(President.eday)
+print(President.mail)
