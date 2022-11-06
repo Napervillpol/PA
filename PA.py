8af8d687893e4b42b478fb4bf4d68c113141e68e
Naperville Nate <napervillepolitics@gmail.com>
2022-11-05 20:43:23 -0500

    added adtotalvotes function

--- PA.py
+++ PA.py
@@ -1,5 +1,6 @@
 
 import pandas as pd
+pd.options.mode.chained_assignment = None
 
 def write_to_excel(race,race_name):
     writer = pd.ExcelWriter('PA_'+race_name+ '.xlsx', engine='xlsxwriter')
@@ -34,6 +35,7 @@ def calculations(df,Dem_name,Rep_name):
     df.insert(6, Rep_name+" Pct", df[Rep_name]/(df[Dem_name]+df[Rep_name]))
     df.insert(7, "Margin",(df[Dem_name]/(df[Dem_name]+df[Rep_name])) -(df[Rep_name]/(df[Dem_name]+df[Rep_name])))
 
+
 def calculate_shift(df_2022,df_2020):
      
      df_2022.mail.insert(8, "Pct Shift",df_2022.mail["Margin"]-df_2020.mail["Margin"])
@@ -47,6 +49,69 @@ def calculate_shift(df_2022,df_2020):
 
      df_2022.total.insert(8, "Pct Shift",df_2022.total["Margin"]-df_2020.total["Margin"])
      df_2022.total.insert(9, "Turnout",df_2022.total["Total"]/df_2020.total["Total"])
+
+def MailProjection(df2,df3):
+    #print(df2.mail)
+    #print(df3)
+    df2.mail= df2.mail.merge(df3,on="County")
+
+def addTotalVotes(df,df1,df2):
+    
+    Senate_race= df.loc[(df['Office Name']  =='United States Senator' )]
+    Governor_race = df.loc[(df['Office Name']  =='Governor' )]
+    
+    Senate_race['Votes'] =  Senate_race['Votes'].astype(str)
+    Senate_race['Votes'] =  Senate_race['Votes'].str.replace(',','')
+    Senate_race['Votes'] =  Senate_race['Votes'].astype(int)
+
+    Senate_race['Election Day Votes'] =  Senate_race['Election Day Votes'].astype(str)
+    Senate_race['Election Day Votes'] =  Senate_race['Election Day Votes'].str.replace(',','')
+    Senate_race['Election Day Votes'] =  Senate_race['Election Day Votes'].astype(int)
+
+    Senate_race['Mail Votes'] =  Senate_race['Mail Votes'].astype(str)
+    Senate_race['Mail Votes'] =  Senate_race['Mail Votes'].str.replace(',','')
+    Senate_race['Mail Votes'] =  Senate_race['Mail Votes'].astype(int)
+
+    Senate_race['Provisional Votes'] =  Senate_race['Provisional Votes'].astype(str)
+    Senate_race['Provisional Votes'] =  Senate_race['Provisional Votes'].str.replace(',','')
+    Senate_race['Provisional Votes'] =  Senate_race['Provisional Votes'].astype(int)
+
+   
+    Governor_race['Votes'] =  Governor_race['Votes'].astype(str)
+    Governor_race['Votes'] =  Governor_race['Votes'].str.replace(',','')
+    Governor_race['Votes'] =  Governor_race['Votes'].astype(int)
+
+    Governor_race['Election Day Votes'] =  Governor_race['Election Day Votes'].astype(str)
+    Governor_race['Election Day Votes'] =  Governor_race['Election Day Votes'].str.replace(',','')
+    Governor_race['Election Day Votes'] =  Governor_race['Election Day Votes'].astype(int)
+
+    Governor_race['Mail Votes'] =  Governor_race['Mail Votes'].astype(str)
+    Governor_race['Mail Votes'] = Governor_race['Mail Votes'].str.replace(',','')
+    Governor_race['Mail Votes'] =  Governor_race['Mail Votes'].astype(int)
+
+    Governor_race['Provisional Votes'] = Governor_race['Provisional Votes'].astype(str)
+    Governor_race['Provisional Votes'] = Governor_race['Provisional Votes'].str.replace(',','')
+    Governor_race['Provisional Votes'] =  Governor_race['Provisional Votes'].astype(int)
+
+
+    Senate_race = Senate_race.groupby(Senate_race['County Name'])[['Votes','Election Day Votes','Mail Votes','Provisional Votes']].sum().reset_index()
+    Governor_race =  Governor_race.groupby(Governor_race['County Name'])[['Votes','Election Day Votes','Mail Votes','Provisional Votes']].sum().reset_index()
+
+    print(Senate_race)
+    print(Governor_race)
+    
+    df1.total['Total'] =Senate_race['Votes']
+    df1.mail['Total'] =Senate_race['Mail Votes']
+    df1.eday['Total'] =Senate_race['Election Day Votes']
+    df1.prov['Total'] =Senate_race['Provisional Votes']
+
+    df2.total['Total'] =Governor_race['Votes']
+    df2.mail['Total'] =Governor_race['Mail Votes']
+    df2.eday['Total'] =Governor_race['Election Day Votes']
+    df2.prov['Total'] =Governor_race['Provisional Votes']
+ 
+  
+
 
 class race:
     mail=[]
@@ -101,8 +166,11 @@ def assign_race(Dem,Rep,Dem_name,Rep_name):
     return Race;
 
 
-df = pd.read_csv('PA.csv')
+#df = pd.read_csv('PA.csv')
 df2= pd.read_csv('PA_2020.csv')
+df3 = pd.read_csv('PA_ABSENTEES.csv')
+df = pd.read_csv('2022General.csv')
+
 
 # 2020 President
 Biden= df2.loc[(df2['Office Name']  =='President of the United States' ) & (df2['Party Name']  =='Democratic' )]
@@ -115,11 +183,18 @@ Oz= df.loc[(df['Office Name']  =='United States Senator' ) & (df['Party Name']  
 Senate = assign_race(Fetterman,Oz,"Fetterman","Oz")
 calculate_shift(Senate,President)
 
+
 # 2022 Gov
 Shapiro= df.loc[(df['Office Name']  =='Governor' ) & (df['Party Name']  =='Democratic' )]
 Mastriano= df.loc[(df['Office Name']  =='Governor' ) & (df['Party Name']  =='Republican' )]
 Governor = assign_race(Shapiro,Mastriano,"Shapiro","Mastriano")
+calculate_shift(Governor,President)
+
+addTotalVotes(df,Senate,Governor)
+MailProjection(Senate,df3)
 
 write_to_excel(President,"President")
 write_to_excel(Senate,"Senate")
 write_to_excel(Governor,"Governor")
+
+
